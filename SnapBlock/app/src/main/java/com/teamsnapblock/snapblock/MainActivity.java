9c8a9e8dbd749efcfe2ef0feed2e0ea035fd2d2e
Ra1nWarden <wzh19921016@gmail.com>
2015-07-17 21:31:41 -0700

    added samples and google material icons

--- SnapBlock/app/src/main/java/com/teamsnapblock/snapblock/MainActivity.java
+++ SnapBlock/app/src/main/java/com/teamsnapblock/snapblock/MainActivity.java
@@ -1,6 +1,8 @@
 package com.teamsnapblock.snapblock;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.GridView;
 
@@ -19,8 +21,16 @@ public class MainActivity extends Activity {
         gridView = (GridView) findViewById(R.id.photos_grid);
         adapter = new PhotosGridAdapter(this, R.layout.photo_grid);
         gridView.setAdapter(adapter);
+        addSamples();
     }
 
-    
+    private void addSamples() {
+        Drawable sampleDrawable = getResources().getDrawable(R.drawable.green_mushroom, null);
+        for (int i = 0; i < 9; i++) {
+            adapter.add(sampleDrawable);
+        }
+    }
+
+
 
 }
