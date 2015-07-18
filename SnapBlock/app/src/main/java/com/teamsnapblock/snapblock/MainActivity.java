dd4a3880f177e3a047510aa84d626bb6ef657a09
Ra1nWarden <wzh19921016@gmail.com>
2015-07-17 21:57:09 -0700

    added camera icon

--- SnapBlock/app/src/main/java/com/teamsnapblock/snapblock/MainActivity.java
+++ SnapBlock/app/src/main/java/com/teamsnapblock/snapblock/MainActivity.java
@@ -1,10 +1,12 @@
 package com.teamsnapblock.snapblock;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
-import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.GridView;
+import android.widget.ImageView;
 
 /**
  * Created by zihao on 7/17/15.
@@ -13,6 +15,7 @@ public class MainActivity extends Activity {
 
     private GridView gridView;
     private PhotosGridAdapter adapter;
+    private ImageView cameraIcon;
 
     @Override
     public void onCreate(Bundle savedInstance) {
@@ -21,7 +24,20 @@ public class MainActivity extends Activity {
         gridView = (GridView) findViewById(R.id.photos_grid);
         adapter = new PhotosGridAdapter(this, R.layout.photo_grid);
         gridView.setAdapter(adapter);
+        setUpNewStory();
         addSamples();
+    }
+
+    private void setUpNewStory() {
+        cameraIcon = (ImageView) findViewById(R.id.camera_icon);
+        cameraIcon.setClickable(true);
+        cameraIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NewStoryActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void addSamples() {
---
+++ SnapBlock/app/src/main/java/com/teamsnapblock/snapblock/NewStoryActivity.java
@@ -0,0 +1,18 @@
+package com.teamsnapblock.snapblock;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by zihao on 7/17/15.
+ */
+public class NewStoryActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        setContentView(R.layout.story_view);
+    }
+
+
+}
